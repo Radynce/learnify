a67cdf9fbab41351f0b8bf9bdd140810b0952f4c
kshetritej <tejghartikshetri@gmail.com>
2024-03-05 14:17:10 +0545

    add: Rosource view and controller

---
+++ Controllers/ResourceController.cs
@@ -0,0 +1,27 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using learnify.Models;
+
+namespace learnify.Controllers;
+
+public class ResourceController : Controller
+{
+    private readonly ILogger<ResourceController> _logger;
+
+    public ResourceController(ILogger<ResourceController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult ViewResource()
+    {
+        return View();
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
