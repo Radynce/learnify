eb44dff838dbc22e64a12f14e05d0a0c905c832a
kshetritej <tejghartikshetri@gmail.com>
2024-03-05 12:46:10 +0545

    add: Resource model

---
+++ Models/Resource.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Resource{
+    [Key]
+    public Guid Id { get; set; }
+    [Required]
+    [MinLength(3,ErrorMessage ="Name must be at least 3 letters long")]
+    public string? Name { get; set;}
+    [Required]
+    [Url(ErrorMessage ="Link must be a valid URL.")]
+    public string? Link { get; set; }
+    [Required]
+    [MinLength(10, ErrorMessage ="Please explain a little bit about the course.")]
+    public string? Description {get; set; }
+    public string? Field { get; set; }
+    [MaxLength(3, ErrorMessage ="Only maximum of 3 tags are allowed.")]
+    public List<string>? Tags { get; set; }
+}
